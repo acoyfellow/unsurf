62183ca4dcedd90dcf42e4985bca856ec46ed101
Jordan Coeyman <coeyman@gmail.com>
2026-02-16 20:34:04 -0500

    feat: Add Footer component to configuration and remove external link from Header
    - Added Footer component to the Astro configuration for better site structure.
    - Removed external link to "coey" from the Header component for a cleaner navigation.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -22,6 +22,7 @@ export default defineConfig({
       components: {
         Head: './src/components/Head.astro',
         Header: './src/components/Header.astro',
+        Footer: './src/components/Footer.astro',
       },
       sidebar: [
         {
